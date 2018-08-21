05b7a4457db8900e9e1c90c5a1acb58b125f15a1
anderscodes <anders.j.persson@hotmail.com>
2018-08-21 23:45:55 +0100

    styling added

--- ipaddress.php
+++ ipaddress.php
@@ -4,6 +4,26 @@
     <meta charset="utf-8">
     <title></title>
     <style type="text/css">
+    body{
+      margin: 0;
+      padding: 0;
+      background: #DFCC89;
+    }
+    #main-content {
+      margin: 30px;
+      text-align: center;
+      color: #3D2399;
+    }
+    #main-content h1{
+      font: 40px Arial, Helvetica, sans-serif;
+    }
+    #main-content p {
+      font: 24px "Times New Roman", Times, Georgia, serif;
+    }
+    #main-content p strong {
+      font-size 70px;
+      color: #000000;
+    }
     </style>
   </head>
   <body>
