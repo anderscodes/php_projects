1c33aa7bdf5c7b26506be61ac4f75354e9052e26
anderscodes <anders.j.persson@hotmail.com>
2018-08-21 22:45:28 +0100

    hello world and ipaddress files created

---
+++ HelloWorld.php
@@ -0,0 +1,34 @@
+class Model {
+  public $text;
+
+  public function __construct() {
+    $this->text = 'Hello world!';
+  }
+}
+
+class View {
+  private $model;
+
+  public function __construct(Model $model) {
+    $this->model = $model;
+  }
+
+  public function output() {
+    return '<h1>' . $this->model->text '</h1>';
+  }
+
+}
+
+class Controller {
+  private $model;
+
+  public function __construct(Model $model) {
+    $this->model = $model;
+  }
+}
+
+$model = new Model();
+
+$controller = new Controller($model);
+$view = new View($model);
+echo $view->output();
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # PHP Projects #
 
 This repo is a series of simple PHP projects that I have decided to do in order to learn some PHP.
-I am using a tutorial on inLearning in order to learn.
+
+I am using a tutorial on inLearning by Kevin Skoglund in order to learn.
+
+These projects are all single-serving websites and they are all esoteric.
