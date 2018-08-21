13f6e6326edb6e5e851d65caac5f2afdf7c79e9c
anderscodes <anders.j.persson@hotmail.com>
2018-08-21 23:45:24 +0100

    final web page implemented

--- ipaddress_filtered.php
+++ ipaddress_filtered.php
@@ -15,11 +15,21 @@ function forwarded_ip() {
        $ip_array = explode(',', $_SERVER[$key]);
        foreach($ip_array as $ip) {
          $ip = trim($ip);
-         return $ip;
+         if(validate_ip($ip)) {
+           return $ip;
+         }
        }
      }
    }
    return '';
+}
+
+function validate_ip($ip) {
+  if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 $remote_ip = $_SERVER['REMOTE_ADDR'];
