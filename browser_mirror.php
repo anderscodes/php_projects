7b2c79c88d0b1a2ffa9a8cdd4971e35ad6ee4130
anderscodes <anders.j.persson@hotmail.com>
2018-08-22 11:42:34 +0100

    created browser_mirror page

---
+++ browser_mirror.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Browser Mirror</title>
+    <style type="text/css">
+    body{
+      margin: 0;
+      padding: 0;
+      background: #DFCC89;
+    }
+    #main-content {
+      margin: 30px;
+      text-align: center;
+      color: #3D2399;
+    }
+    #main-content h1{
+      font: 40px Arial, Helvetica, sans-serif;
+    }
+    #main-content p {
+      font: 24px "Times New Roman", Times, Georgia, serif;
+    }
+    #main-content p strong {
+      font-size 70px;
+      color: #000000;
+    }
+    </style>
+  </head>
+  <body>
+    <div id='main-content'>
+      <h1>Browser Mirror</h1>
+
+    </div>
+  </body>
+</html>
