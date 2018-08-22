b612c77fe6462ef621474afab14be5f1626ee612
anderscodes <anders.j.persson@hotmail.com>
2018-08-22 11:52:49 +0100

    fields added to browser_mirror

--- browser_mirror.php
+++ browser_mirror.php
@@ -30,6 +30,20 @@
     <div id='main-content'>
       <h1>Browser Mirror</h1>
 
+        <p>Remote IP: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+        <p>User Agent: <?php echo $_SERVER['HTTP_USER_AGENT']; ?></p>
+        <p>Referer: <?php echo $_SERVER['HTTP_REFERER']; ?></p>
+        <p>Request Time: <?php echo $_SERVER['REQUEST_TIME']; ?></p>
+        <p>Request URI: <?php echo $_SERVER['REQUEST_URI']; ?></p>
+        <p>Request Method: <?php echo $_SERVER['REQUEST_METHOD']; ?></p>
+        <p>Query String: <?php echo $_SERVER['QUERY_STRING']; ?></p>
+        <p>HTTP Accept: <?php echo $_SERVER['HTTP_ACCEPT']; ?></p>
+        <p>HTTP Accept Charset: <?php echo $_SERVER['HTTP_ACCEPT_CHARSET']; ?></p>
+        <p>HTTP Accept Encoding: <?php echo $_SERVER['HTTP_ACCEPT_ENCODING']; ?></p>
+        <p>HTTP Accept Language: <?php echo $_SERVER['HTTP_ACCEPT_LANGUAGE']; ?></p>
+        <p>HTTPS?: <?php echo $_SERVER['HTTPS']; ?></p>
+        <p>Remote Port: <?php echo $_SERVER['REMOTE_PORT']; ?></p>
+
     </div>
   </body>
 </html>
